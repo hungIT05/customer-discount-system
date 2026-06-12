b572f368351baf9f337d02ea93fbbe6128bb586f
Pham Viet Hung <hungpham.@gmail.com>
2026-06-12 14:33:31 +0700

    Implement loyalty customer discount feature

---
+++ discount.py
@@ -0,0 +1,4 @@
+def discount (order_before):
+    if order_before >= 50000000:
+        return 0.1
+    return 0
---
+++ test_discount.py
@@ -0,0 +1,5 @@
+from discount import discount 
+def test_case1():
+    assert discount(50000000) == 0.1
+def test_case2():    
+    assert discount(50000000) == 0
